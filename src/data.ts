8dcae77889032326b01a359dde1addc150ee44e2
Diego M. Pousa <diegopousalp@gmail.com>
2025-11-25 10:02:10 -0300

    Projects section

--- src/data.ts
+++ src/data.ts
@@ -1,10 +1,16 @@
 import dayjs from "dayjs";
 
+
 export default {
   experience: (new Array(4)).fill("").map((_, i) => ({
     title: "Company " + i,
     fromDate: dayjs().add(i, "days"),
     toDate: dayjs().add(i, "days"),
     description: "Nice job :happy: " + i
+  })),
+  projects: (new Array(4)).fill("").map((_, i) =>( {
+    name: "Project " + i,
+    description: "Short description of the project " + i,
+    technologies: (new Array(i)).fill("").map((_, j) => "Tech " + j)
   }))
 }
