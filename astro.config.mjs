d5d4ed1a6618642b3a4218b6d1c25e9853edaf06
Diego M. Pousa <diegopousalp@gmail.com>
2025-12-03 17:26:12 -0300

    Added a better way to import files with absolute paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path from "path";
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
+    },
+  },
+  i18n: {
+    locales: ["es", "en"],
+    defaultLocale: "en"
   }
 });
